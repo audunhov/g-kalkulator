cec0c0ce2331fdc91ce0756d020f0c9c0f28bc46
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-06-24 12:24:11 +0200

    debugging

--- pages/index.vue
+++ pages/index.vue
@@ -5,35 +5,29 @@
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-import Calculator from '~/components/Calculator.vue'
+<script>
+import Vue from "vue";
+import Calculator from "~/components/Calculator.vue";
 
 export default Vue.extend({
-    name: "IndexPage",
-    components: { Calculator },
-    data() {
-      return {
-        g: null,
-        date: null,
-      }
-    },
-    methods: {
-      async load() {
-        let response = await fetch("https://g.nav.no/api/v1/historikk")
-        let data = await response.json()
-        this.g = data[0]["grunnbeløp"]
-        this.date = data[0]["dato"]
-      }
-    },
-    created() {
-      this.load()
-    }
-})
+  name: "IndexPage",
+  components: { Calculator },
+  data() {
+    return {
+      g: null,
+      date: null,
+    };
+  },
+  async created() {
+    let response = await fetch("https://g.nav.no/api/v1/historikk");
+    let data = await response.json();
+    this.g = data[0]["grunnbeløp"];
+    this.date = data[0]["dato"];
+  },
+});
 </script>
 
 <style>
-
 * {
   box-sizing: border-box;
 }
@@ -52,5 +46,4 @@ export default Vue.extend({
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
 }
-
 </style>
