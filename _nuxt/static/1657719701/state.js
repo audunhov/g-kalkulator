2c55b89a2ce8d76eda6fd2f442c42269cc182edd
Audun Hammer Hovda <hammerhovda@gmail.com>
2022-07-13 15:41:42 +0200

    62f8757

---
+++ _nuxt/static/1657719701/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fg-kalkulator\u002F_nuxt\u002Fstatic\u002F1657719701",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fg-kalkulator\u002F",assetsPath:"\u002Fg-kalkulator\u002F_nuxt\u002F",cdnURL:a}}}}(null));
